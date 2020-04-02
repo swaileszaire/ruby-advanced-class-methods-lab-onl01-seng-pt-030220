811d4d4903181234136a6d8ccbf54f01434fed58
Zaire  Swailes <swaileszaire@gmail.com>
2020-04-02 06:25:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -87,7 +87,11 @@ class Song
   end
 
   def create_from_filename 
-    
+     song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    @@all << song
+    song
     #read description
   end
   
